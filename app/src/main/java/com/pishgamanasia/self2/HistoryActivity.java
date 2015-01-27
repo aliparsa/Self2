e20f7c8a0783fea3a51db4ecc25845ae7682ae05
parsa <ali.parsa.p@gmail.com>
2015-01-27 09:22:48 +0330

    some changes

--- app/src/main/java/com/pishgamanasia/self2/HistoryActivity.java
+++ app/src/main/java/com/pishgamanasia/self2/HistoryActivity.java
@@ -1,10 +1,15 @@
 package com.pishgamanasia.self2;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
+import com.pishgamanasia.self2.Adapter.ListViewObjectAdapter;
 import com.pishgamanasia.self2.DataModel.YearMonthItem;
 import com.pishgamanasia.self2.Helper.DateHelper;
 import com.pishgamanasia.self2.Helper.PersianCalendar;
@@ -15,26 +20,49 @@ import java.util.List;
 
 public class HistoryActivity extends Activity {
 
+
+    private Context context;
+    ListView yearListview;
+    ListView mountListview;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
+        context= this;
+
+        // init
+        yearListview = (ListView) findViewById(R.id.sallistView);
+        mountListview = (ListView) findViewById(R.id.mahListview);
+
+        // lets start
+        fillYearListVIew();
     }
 
 
     private void fillYearListVIew(){
 
         List<YearMonthItem> years = DateHelper.getYearsBefore(new PersianCalendar(), 5);
-
-
+        ListViewObjectAdapter adapter = new ListViewObjectAdapter(context,years);
+        yearListview.setAdapter(adapter);
+        yearListview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                PersianCalendar persianCalendar = ((YearMonthItem.Holder) view.getTag()).getYearmonthitem().getDate();
+                fillMonthListVIew(persianCalendar);
+            }
+        });
 
     }
+
+
 
 
     private void fillMonthListVIew(PersianCalendar date){
 
         List<YearMonthItem> months = DateHelper.getMonthsOfYear(date);
-
+        ListViewObjectAdapter adapter = new ListViewObjectAdapter(context,months);
+        mountListview.setAdapter(adapter);
 
 
     }
